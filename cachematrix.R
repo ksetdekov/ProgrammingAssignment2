026f27ca73183e7a0662054bfeee33138e82447a
ksetdekov <ksetdekov@gmail.com>
2018-05-03 13:20:01 +0300

    finished working functions
    finished working functions and added testing routine that multiplies the result of the cacheSolve by the initial random matrix of 5*5 to test if this is actually an inverted matrix. X*X^(-1)=I

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 ##  This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
@@ -16,12 +26,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
 
 
 ## Code for testing
 w<-matrix(sample(1:25,replace = FALSE),nrow = 5, ncol = 5)
-w_inv<-solve(w)
-wres<-w %*% w_inv
+y<-makeCacheMatrix(w)
+print("first calulation of inverse - not cached")
+print(cacheSolve(y))
+print("second calulation of inverse - already cached")
+w_inv<-cacheSolve(y)
+print(w_inv)
+
+## multiplication to test if results of cacheSolve is an inverse matix
+wres<-w %*% w_inv 
 ## test for inversion being true
-round(sum(wres),digits = 6)==dim(wres)[1]
+if (dim(wres)[1]==round(sum(wres),digits = 6)) {
+        print("Inversion is correct and W*W-1 equals unity matrix")
+}else{
+        print("Inversion seems not correct and W*W-1 does not equal unity matrix")
+}
